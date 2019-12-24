d191a70eca2dfdacbdae3c4e636177eb9cc9ecb0
Dmitriy Kuzichkin <sxespade@gmail.com>
2019-12-24 12:50:11 +0300

    initial commit.'

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    String[] name = {"Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+            "Костя", "Игорь"};
+    String[] position = {"Программер", "Бухгалтер", "Программер",
+            "Программер", "Бухгалтер", "Директор", "Программер", "Охранник"};
+    int salary[] = {13000, -10000, 13000, 13000, 10000, -15000, 13000, 8000};
+
+    int[] colors = new int[3];
+
+    /**
+     * Called when the activity is first created.
+     */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        colors[0] = Color.parseColor("#559966CC");
+        colors[1] = Color.parseColor("#55336699");
+        colors[2] = Color.parseColor("#b62006");
+
+        LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
+
+        LayoutInflater ltInflater = getLayoutInflater();
+
+        for (int i = 0; i < name.length; i++) {
+            Log.d("myLogs", "i = " + i);
+            View item = ltInflater.inflate(R.layout.item, linLayout, false);
+            TextView tvName = (TextView) item.findViewById(R.id.tvName);
+            tvName.setText(name[i]);
+
+            if (salary[i] < 0) {tvName.setTextColor(getResources().getColor(R.color.black));}
+            else{}
+
+
+            TextView tvPosition = (TextView) item.findViewById(R.id.tvPosition);
+            tvPosition.setText("Должность: " + position[i]);
+            TextView tvSalary = (TextView) item.findViewById(R.id.tvSalary);
+            tvSalary.setText("Оклад: " + String.valueOf(salary[i]));
+            item.getLayoutParams().width = LayoutParams.MATCH_PARENT;
+
+            if (salary[i] < 0) {
+
+                item.setBackgroundColor(colors[2]);
+                linLayout.addView(item);
+            } else {
+                item.setBackgroundColor(colors[i % 2]);
+                linLayout.addView(item);
+            }
+
+        }
+
+    }
+}
